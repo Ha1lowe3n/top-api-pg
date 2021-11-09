d345e16cc69486c6a15354dfd7f1ecadc71d57ae
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-11-10 01:26:17 +0800

    connect to db, create ormconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { ProductModule } from './product/product.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ReviewModule } from './review/review.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './ormconfig';
 
 @Module({
-    imports: [ProductModule, TopPageModule, ReviewModule, AuthModule],
+    imports: [
+        TypeOrmModule.forRoot(config),
+        ProductModule,
+        TopPageModule,
+        ReviewModule,
+        AuthModule,
+    ],
 })
 export class AppModule {}
---
+++ src/ormconfig.ts
@@ -0,0 +1,18 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/modules/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    migrations: [__dirname + '/migrations/*{.ts,.js}'],
+    cli: {
+        migrationsDir: 'src/migrations',
+    },
+};
+
+export default config;
